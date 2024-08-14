5ed34ef8703fd7c94f04dfb3dfc1cdf439916a46
GHAITH_PC\sadra <sadraaandaz@gmail.com>
2024-08-14 15:43:29 +0300

    test

--- lib/feuters/login/ui/onboarding/onboarding_screen.dart
+++ lib/feuters/login/ui/onboarding/onboarding_screen.dart
@@ -27,7 +27,7 @@ class OnboardingScreen extends StatelessWidget {
                   padding:  EdgeInsets.symmetric(horizontal: 20.h),
                   child: Column(
                     children: [
-                      Text('Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                      Text('Manage and schedule all of your medical appointments easily with Docdoc to get a new experience .',
                       style: TextStyles.font13GrayRegular,textAlign: TextAlign.center,),
                       SizedBox(height: 30.h,),
                       GetStartedButton(() {
