be781c36566161bcc72981b659833f0df25703f0
GHAITH_PC\sadra <sadraaandaz@gmail.com>
2024-08-15 12:43:35 +0300

    no message

--- lib/feuters/login/ui/sing_up/widgets/sing_up_lisetener.dart
+++ lib/feuters/login/ui/sing_up/widgets/sing_up_lisetener.dart
@@ -29,7 +29,7 @@ class SingUpLisetener extends StatelessWidget {
                 context: context,
                 builder: (contextbuilder) => const Center(
                       child: CircularProgressIndicator(
-                        color: Color.fromARGB(255, 0, 0, 0),
+                        color: Color.fromARGB(255, 255, 255, 255),
                       ),
                     ));
           },
