03e5c5f347e0b11d76cec774622fa23f6f9b95b7
GHAITH_PC\sadra <sadraaandaz@gmail.com>
2024-08-16 16:56:16 +0300

    add save token and secure token

--- lib/feuters/login/ui/home/ui/widgets/specializations_and_doctors_bloc_builder.dart
+++ lib/feuters/login/ui/home/ui/widgets/specializations_and_doctors_bloc_builder.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_complete_project/core/helpers/spacing.dart';
+import 'package:flutter_complete_project/feuters/login/ui/home/data/models/specializations_response_model.dart';
 import 'package:flutter_complete_project/feuters/login/ui/home/logic/cubit/home_cubit.dart';
 import 'package:flutter_complete_project/feuters/login/ui/home/logic/cubit/home_state.dart';
 import 'package:flutter_complete_project/feuters/login/ui/home/ui/widgets/doctor_list.dart';
@@ -16,15 +17,17 @@ class SpecializationsAndDoctorsBlocBuilder extends StatelessWidget {
     return BlocBuilder<HomeCubit, HomeState>(
       buildWhen: (previous, current) =>
           current is SpecializationsLoading ||
-          current is SpecializationsLoading ||
-          current is SpecializationsLoading,
+          current is SpecializationsSuccess ||
+          current is SpecializationsError,
       builder: (context, state) {
         return state.maybeWhen(
           specializationsLoading: () {
             return setupLoading();
-          }, 
+          },
           specializationsSuccess: (specializationsResponseModel) {
-            var specializationList = specializationsResponseModel.specializationDataList;
+            var specializationList =
+                specializationsResponseModel.specializationDataList;
+            print(specializationList!.length);
             return setupSuccess(specializationList);
           },
           specializationsError: (errorHandler) {
@@ -47,15 +50,17 @@ class SpecializationsAndDoctorsBlocBuilder extends StatelessWidget {
     );
   }
 
-  Widget setupSuccess(specializationList) {
-    return Column(
-      children: [
-        vericalSpace(13.h),
-        DoctorSpecialityViewAll(specializationList ?? []),
-        DoctorRecommendation(),
-        vericalSpace(13.h),
-        DoctorList(),
-      ],
+  Widget setupSuccess(List<SpecializationsData?> specializationList) {
+    return Expanded(
+      child: Column(
+        children: [
+          vericalSpace(13.h),
+          DoctorSpecialityViewAll(specializationList),
+          DoctorRecommendation(),
+          vericalSpace(13.h),
+          DoctorList(doctorsList: specializationList.first?.doctorsList,),
+        ],
+      ),
     );
   }
 }
